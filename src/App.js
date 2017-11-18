e3ed2d91bbf55ef8774c02889be94a7f47b84879
nikodunk <n.dunkel@gmail.com>
2017-11-17 21:30:10 -0800

    first ocmmit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery'; 
 
 class App extends Component {
+
+  componentDidMount() {
+    $.getJSON("http://localhost:5000/data.json", function(result){
+            console.log('this works and is the JSON:' + result )
+         })
+  } 
+
+
+
   render() {
     return (
       <div className="App">
@@ -10,13 +20,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className="subtitle">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-
       </div>
     );
   }
+
+  
 }
 
 export default App;
