58bbe108d31b5b9276e654ab483ed7f06861f5c0
nikodunk <n.dunkel@gmail.com>
2017-11-18 14:16:53 -0800

    now works

--- src/App.js
+++ src/App.js
@@ -13,40 +13,22 @@ class App extends Component {
     super(props);
 
     this.state = {
-        'Money': 'test',
-        'Emissions Per Year (GHP)': 200,
-        'Start_Year': 2017,
-        'Curr_Year': 2017,
-        'Solar_Investment': 0,
-        'Wind_Investment': 0,
-        'Nuclear_Investment': 0,
-        'Gas_Investment': 0,
-        'GDP': 18566900000000,
-        'Sea_Levels': 0,
-        'Electricity_Price': 0,
-        'Probability_of_violence': 0,
-        'Agriculture': 0,
-        'Risk_of_Hurricane': 0,
-        'Game_Over': false
     }
       
   }
 
   componentDidMount() {
-
-    this.getRequest()
-    
+      this.getData()  
   }
 
-  getRequest(){
-      fetch("http://localhost:5000/")
-      .then(results => {return results.json()} )
-      .then( data => this.setState({result: data}))
 
-      console.log('state' + this.state.result)
-   }
-
-
+  getData(){
+    var url = "http://127.0.0.1:5000/"
+        $.get(url).then(result => {
+            this.setState(JSON.parse(result))
+          })
+        console.log(this.state)
+  }
 
 
   onClick(e){
@@ -60,15 +42,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" /><h1 className="App-title">ClimateChase</h1>
         </header>
-        <h1 className="subtitle" id="GDP"> GDP: ${this.state.GDP} </h1>
-        <h1 className="subtitle" id="GDP"> GDP: ${this.state.GDP} </h1>
-        <h1 className="subtitle" id="Year">{this.state.Curr_Year} </h1>
-        <h1 className="subtitle" id="Year">Finances: ${this.state.Money} </h1>
-        invest<br />
-        <button onClick={this.onClick.bind(this)} id="wind">$1=>windpower</button><br />
-        <button onClick={this.onClick.bind(this)} id="solar">$1=>solar</button><br />
-        <button onClick={this.onClick.bind(this)} id="nuclear">$1=>nuclear</button><br />
-        <button onClick={this.onClick.bind(this)} id="fossil">$1=>fossil</button><br />
+        <h1 className="subtitle" > {this.state.Curr_Year}</h1>
+        <h1 className="subtitle" > GDP: ${this.state.GDP}</h1>
+        <h1 className="subtitle" >Finances: ${this.state.Money}</h1>
+        <hr />
+        <h2>invest in</h2>
+        <button onClick={this.onClick.bind(this)} id="wind">$1=>windpower</button>
+        <button onClick={this.onClick.bind(this)} id="solar">$1=>solar</button>
+        <button onClick={this.onClick.bind(this)} id="nuclear">$1=>nuclear</button>
+        <button onClick={this.onClick.bind(this)} id="fossil">$1=>fossil</button>
       </div>
     );
   }
