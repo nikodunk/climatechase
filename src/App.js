fcbf75e1eb3dc5f4e7450f255e6a2bee546b10e2
nikodunk <n.dunkel@gmail.com>
2017-11-18 16:33:04 -0800

    supernice

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" /><h1 className="App-title">ClimateChase</h1>
+            <h1 className="App-title">ClimateChase</h1>
         </header>
         <div>
           year <br />
