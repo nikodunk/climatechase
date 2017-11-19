a5270311a465ad0fb098cf99d3e60560289e7ec9
Rohan Pandey <rohan@calvisitor-10-105-152-117.calvisitor.1918.berkeley.edu>
2017-11-18 16:40:54 -0800

    finished gdp growth stuff

--- app.py
+++ app.py
@@ -26,21 +26,15 @@ jsonObject = {
         'Electricity_Price': 0,
         'Agriculture': 0,
 
-        'Risk_of_Hurricane': 0,
+        #'Risk_of_Hurricane': 0,
         'Hurricanes_Happen': False,
-        'Number_of_Hurricanes': 0,
+        #'Number_of_Hurricanes': 0,
 
-        'Risk_of_Riots': 0,
-        'Riots_Happen': False,
-        'Number_of_Riots': 0,
+        #'Risk_of_Riots': 0,
+        'AC_Happen': False,
+        #'Number_of_Riots': 0,
 
-        'Risk_of_Famines': 0,
-        'Famines_Happen': False,
-        'Number_of_Famines': 0,
-
-        'Risk_of_Recessions': 0,
-        'Recessions_Happen': False,
-        'Number_of_Recessions': 0,
+        'GDP_Growth': 0,
 
         'GHG': 0,
         'Game_Over': False
@@ -65,14 +59,14 @@ def update_climate(jsonObject):
         jsonObject['Game_Over'] = True
         return "Global Warming has overtaken the world. Humanity cannot continue."
     jsonObject['Curr_Year'] += 1
-    jsonObject['GDP'] = round(jsonObject['GDP'] * 1.01, 2)
+    jsonObject['GDP'] = round(jsonObject['GDP'] * 1.01 + jsonObject['GDP_Growth'], 2)
     jsonObject['Money'] = round(jsonObject['Money'] + jsonObject['GDP'] * .01, 2)
     jsonObject['GHG'] = update_ghg(jsonObject, input_to_ghg_map)[1]
     curr_ghg = jsonObject['GHG']
-    hurCalc(curr_ghg) = jsonObject['']
-    ACCalc(curr_ghg, jsonObject['fossil'], jsonObject['nuclear']) = jsonObject['']
+    hurCalc(curr_ghg) = jsonObject['Hurricanes_Happen']
+    ACCalc(curr_ghg, jsonObject['fossil'], jsonObject['nuclear']) = jsonObject['AC_Happen']
     agriCalc(curr_ghg, jsonObject['fossil']) = jsonObject['']
-    ecoCalc(curr_ghg, jsonObject['solar'], jsonObject['wind'], jsonObject['nuclear'], jsonObject['fossil']) = jsonObject['']
+    ecoCalc(curr_ghg, jsonObject['solar'], jsonObject['wind'], jsonObject['nuclear'], jsonObject['fossil']) = jsonObject['GDP_Growth']
 
     return jsonObject
 
@@ -88,12 +82,6 @@ def ACCalc(carbon, fSpent, nSpent):
     carbon += twoSpent/degree + x
     return carbon > 1
     # if true, armed conflict occurs
-
-def agriCalc(carbon, fSpent):
-    x = random.random()
-    carbon += fSpent/degree + x
-    return carbon
-    # adds to economy growth
 
 def ecoCalc(carbon, sSpent, wSpent, nSpent, fSpent):
     x = random.random()
@@ -123,10 +111,10 @@ def lose(jsonObject):
     if jsonObject['Game_Over'] or jsonObject['GDP'] < 0 or jsonObject['Money'] < 0:
         return True
     if jsonObject['Curr_Year'] - jsonObject['Start_Year'] > 500:
-        return jsonObject['GHG'] > 1000000
+        return jsonObject['GHG'] > .7
 
 def win(jsonObject):
-    return jsonObject['GHG'] < 10000
+    return jsonObject['GHG'] < 10000:
 
 #########################################################
 # Routes
